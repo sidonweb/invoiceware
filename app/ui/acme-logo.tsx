06f70d381dde1a97bee8d55505a04ca809fca5bb
SiddharthCodes <95767952+SiddharthCodes@users.noreply.github.com>
2024-11-12 14:46:48 +0530

    init commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,14 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { BanknotesIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function InvoicewareLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <BanknotesIcon className="h-12 w-12 rotate-[15deg] mr-2" />
+      <p className="text-[44px]">Invoiceware</p>
     </div>
   );
 }
+
